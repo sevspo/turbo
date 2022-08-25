d8b9e7da3b8b1d6dd4dcca2b28684fd7e15ccfb5
Severin Spoerri <severin.spoerri@outlook.com>
2022-08-26 01:02:25 +0200

    linting working for both projects

--- packages/config/eslint-nest.js
+++ packages/config/eslint-nest.js
@@ -5,12 +5,10 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
-  root: true,
   env: {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
